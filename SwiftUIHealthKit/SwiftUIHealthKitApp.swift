90453ef7b23691d8ec11e502c19f150ecc15bd1b
Quang Pham <phamvanquangit@gmail.com>
2022-08-01 10:43:13 +0700

    Initial Commit

---
+++ SwiftUIHealthKit/SwiftUIHealthKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHealthKitApp.swift
+//  SwiftUIHealthKit
+//
+//  Created by Quang Pham on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHealthKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
